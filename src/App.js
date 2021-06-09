a7ee25fb90b13f44b44c7b37954242f4a531dcc3
AlanSoto31 <soto.alan@hotmail.com>
2021-06-09 11:23:02 -0500

    Install linters and github actions

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
-import React from 'react'
+import React from 'react';
 
-const App = () => {
-  return (
-    <div>
-      <h1>Hello</h1>
-    </div>
-  )
-}
+const App = () => (
+  <div>
+    <h1>Hello</h1>
+  </div>
+);
 
 export default App;
-
