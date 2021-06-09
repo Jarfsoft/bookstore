c07a6fa7243869c2fecde8ef13ed6af76a1f6e95
Jarfsoft <andresjex@hotmail.com>
2021-06-09 14:32:42 -0600

    Render a stateless booklist

--- src/components/BooksList.js
+++ src/components/BooksList.js
@@ -1,9 +1,36 @@
 import React from 'react';
+
 // import PropTypes from 'prop-types';
 
-const BooksList = () => (
-  <div />
-);
+const state = {
+  books: [
+    { bookID: 1, title: 'Book1', category: 'Fiction' },
+    { bookID: 2, title: 'Book2', category: 'Fiction' },
+    { bookID: 3, title: 'Book3', category: 'Fiction' },
+  ],
+};
+
+const BooksList = () => {
+  const row = state.books.map((book) => (
+    <tr key={book.bookID}>
+      <td>{book.bookID}</td>
+      <td>{book.title}</td>
+      <td>{book.category}</td>
+    </tr>
+  ));
+  return (
+    <div>
+      <table>
+        <tr>
+          <th>Book ID</th>
+          <th>Title</th>
+          <th>Category</th>
+        </tr>
+        {row}
+      </table>
+    </div>
+  );
+};
 
 // BooksList.propTypes = {
 
