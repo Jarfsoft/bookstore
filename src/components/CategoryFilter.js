72f206439cbd051f191858163251ac63de35fc3d
AlanSoto31 <soto.alan@hotmail.com>
2021-06-10 17:21:49 -0500

    s

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,24 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const CategoryFilter = ({ onFilterChange }) => {
-  const handleFilterChange = (value) => {
-    onFilterChange(value);
-  };
-
-  return (
-    <select name="categoryFilter" required id="categoryFilter" onChange={(e) => handleFilterChange(e.target.value)}>
-      <option value="ALL">All</option>
-      <option value="action">Action</option>
-      <option value="biography">Biography</option>
-      <option value="history">History</option>
-      <option value="horror">Horror</option>
-      <option value="kids">Kids</option>
-      <option value="learning">Learning</option>
-      <option value="sci-fi">Sci-Fi</option>
-    </select>
-  );
-};
+const CategoryFilter = ({ onFilterChange }) => (
+  <select name="categoryFilter" required id="categoryFilter" onChange={(e) => onFilterChange(e.target.value)}>
+    <option value="ALL">All</option>
+    <option value="action">Action</option>
+    <option value="biography">Biography</option>
+    <option value="history">History</option>
+    <option value="horror">Horror</option>
+    <option value="kids">Kids</option>
+    <option value="learning">Learning</option>
+    <option value="sci-fi">Sci-Fi</option>
+  </select>
+);
 
 CategoryFilter.propTypes = {
   onFilterChange: PropTypes.func.isRequired,
