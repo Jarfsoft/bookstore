41f2425283e54c641a6f7b83d58c744dfa4cf9fd
Jarfsoft <andresjex@hotmail.com>
2021-06-12 07:05:30 -0600

    Change files location, remove hello from app

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
 
-import BooksList from './components/BooksList';
-import BooksForm from './components/BooksForm';
+import BooksList from './BooksList';
+import BooksForm from './BooksForm';
 
 const App = () => (
   <div>
-    <h1>Hello</h1>
     <BooksForm />
     <BooksList />
   </div>
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import App from './App';
+import App from './components/App';
 import rootReducer from './reducers/index';
 import './index.css';
 /* eslint-disable no-underscore-dangle */
